a67e0d8709d17cedacbd7ac1052cfe983fed53ba
elenitach <lena.01460@gmail.com>
2023-11-26 11:15:36 +0500

    add logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,14 +3,19 @@ import styles from './Header.module.css';
 import cn from 'classnames';
 import logo from '../../images/logo.svg';
 import ButtonLink from "../ButtonLink/ButtonLink";
+import { Stack } from "@mui/material";
+import ApartmentOutlinedIcon from "@mui/icons-material/ApartmentOutlined";
 
 const Header = ({loginButtonRequired}) => {
   return (
-    <header className={cn('container', styles.header)}>
-      <Link to={'/'}>
-        Навигатор чистоты
+    <header className={cn("container", styles.header)}>
+      <Link to={"/"}>
+        <Stack direction="row" gap={1} alignItems="center">
+          <ApartmentOutlinedIcon fontSize="large" />{" "}
+          <span>Навигатор чистоты</span>
+        </Stack>
       </Link>
-      {loginButtonRequired && <ButtonLink link={'/login'}>Log In</ButtonLink>}
+      {loginButtonRequired && <ButtonLink link={"/login"}>Log In</ButtonLink>}
     </header>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,25 +2,39 @@ import { Link } from "react-router-dom";
 import NavBarItem from "../NavBarItem/NavBarItem";
 import logo from "../../images/logo.svg";
 import styles from "./Navbar.module.css";
+import ApartmentOutlinedIcon from "@mui/icons-material/ApartmentOutlined";
+import { Stack } from "@mui/material";
 
 const NavBar = () => {
   const user = localStorage.getItem('access_token');
   return (
     <div className={styles.sidebar}>
-      <Link to="/news">Навигатор чистоты</Link>
+      <Link to="/news">
+        <Stack direction="row" gap={1} alignItems="center" >
+          <ApartmentOutlinedIcon fontSize="large" /> <span>Навигатор чистоты</span>
+        </Stack>
+      </Link>
       <ul className={styles.nav}>
         <NavBarItem link={"/news"} icon={"overview"} text={"Новости"} />
         {user && (
-        <NavBarItem link={"/post"} icon={"settings"} text={"Оставить новость"} />
+          <NavBarItem
+            link={"/post"}
+            icon={"settings"}
+            text={"Оставить новость"}
+          />
         )}
         {user && (
-        <NavBarItem link={"/reports"} icon={"overview"} text={"Обращения"} />
+          <NavBarItem link={"/reports"} icon={"overview"} text={"Обращения"} />
         )}
         {user && (
-          <NavBarItem link={"/report"} icon={"recipes"} text={"Сообщить о проблеме"} />
+          <NavBarItem
+            link={"/report"}
+            icon={"recipes"}
+            text={"Сообщить о проблеме"}
+          />
         )}
         {user && (
-        <NavBarItem link={"/map"} icon={"meal"} text={"Карта обращений"} />
+          <NavBarItem link={"/map"} icon={"meal"} text={"Карта обращений"} />
         )}
       </ul>
     </div>
